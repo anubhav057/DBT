f25b34d01fee06af71c772280eca1b36e52b8b94
Anubhav Tyagi <anubhavtyagi057@gmail.com>
2025-11-11 09:36:44 +0000

    v6

---
+++ snapshots/snapshot_tickets_check.sql
@@ -0,0 +1,14 @@
+{% snapshot snapshot_tickets_check%}
+{{
+    config(
+        target_schema='my_snapshots',
+        strategy='check',
+        unique_key='ticket_id',
+        check_cols=['ticket_status'],
+        invalidate_hard_deletes=true
+    )
+}}
+
+select * from {{ref('tickets')}}
+
+{% endsnapshot %}
